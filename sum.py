443b5ebfa3ee2bee6d9b575bfcf323867814334c
Himanshu <himanshu0952000@gmail.com>
2024-12-28 20:34:00 +0530

    directory updated

--- sum.py
+++
@@ -1,8 +0,0 @@
-def say_hello(name):
-    return f"Hello {name}"
-
-def be_awesome(name):
-    return f"Yo {name}, together we're the awesomest!"
-
-def greet_bob(greeter_func):
-    return greeter_func("Bob")
